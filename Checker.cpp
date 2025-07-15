a42bdbd9a47a82ea1cfe6650e8be835d476f5c9b
Muddivish13524 <vishwanathmuddi@gmail.com>
2025-07-15 11:45:08 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -1,22 +1,44 @@
-#include <assert.h>
 #include <iostream>
+#include <tuple>
+
 using namespace std;
 
-bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    cout << "Temperature out of range!\n";
-    return false;
-  } else if(soc < 20 || soc > 80) {
-    cout << "State of Charge out of range!\n";
-    return false;
-  } else if(chargeRate > 0.8) {
-    cout << "Charge Rate out of range!\n";
-    return false;
-  }
-  return true;
+enum class BatteryStatus {
+    OK,
+    TemperatureOutOfRange,
+    SOCOutOfRange,
+    ChargeRateOutOfRange
+};
+
+BatteryStatus checkBatteryStatus(float temperature, float soc, float chargeRate) {
+    if (temperature < 0 || temperature > 45) {
+        return BatteryStatus::TemperatureOutOfRange;
+    }
+    if (soc < 20 || soc > 80) {
+        return BatteryStatus::SOCOutOfRange;
+    }
+    if (chargeRate > 0.8) {
+        return BatteryStatus::ChargeRateOutOfRange;
+    }
+    return BatteryStatus::OK;
+}
+
+const char* statusToString(BatteryStatus status) {
+    switch (status) {
+        case BatteryStatus::OK: return "Battery is OK";
+        case BatteryStatus::TemperatureOutOfRange: return "Temperature out of range!";
+        case BatteryStatus::SOCOutOfRange: return "State of Charge out of range!";
+        case BatteryStatus::ChargeRateOutOfRange: return "Charge Rate out of range!";
+        default: return "Unknown status";
+    }
 }
 
 int main() {
-  assert(batteryIsOk(25, 70, 0.7) == true);
-  assert(batteryIsOk(50, 85, 0) == false);
+    auto status = checkBatteryStatus(25, 70, 0.7);
+    cout << statusToString(status) << endl;
+
+    status = checkBatteryStatus(50, 85, 0);
+    cout << statusToString(status) << endl;
+
+    return 0;
 }
