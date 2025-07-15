d732b77825f14cd871ee9e86b6db1605604b59a9
Muddivish13524 <vishwanathmuddi@gmail.com>
2025-07-15 12:48:40 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <tuple>
+#include <vector>
+#include <functional>
 
 using namespace std;
 
@@ -10,15 +11,22 @@ enum class BatteryStatus {
     ChargeRateOutOfRange
 };
 
+struct BatteryCheck {
+    function<bool(float, float, float)> condition;
+    BatteryStatus status;
+};
+
 BatteryStatus checkBatteryStatus(float temperature, float soc, float chargeRate) {
-    if (temperature < 0 || temperature > 45) {
-        return BatteryStatus::TemperatureOutOfRange;
-    }
-    if (soc < 20 || soc > 80) {
-        return BatteryStatus::SOCOutOfRange;
-    }
-    if (chargeRate > 0.8) {
-        return BatteryStatus::ChargeRateOutOfRange;
+    static const vector<BatteryCheck> checks = {
+        { [](float t, float, float) { return t < 0 || t > 45; }, BatteryStatus::TemperatureOutOfRange },
+        { [](float, float s, float) { return s < 20 || s > 80; }, BatteryStatus::SOCOutOfRange },
+        { [](float, float, float c) { return c > 0.8; }, BatteryStatus::ChargeRateOutOfRange }
+    };
+
+    for (const auto& check : checks) {
+        if (check.condition(temperature, soc, chargeRate)) {
+            return check.status;
+        }
     }
     return BatteryStatus::OK;
 }
